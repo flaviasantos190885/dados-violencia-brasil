d87bfaf0acdba45a4dcfcf46441ac9724944c970
flaviasantos190885 <flaviasantos190885@gmail.com>
2025-09-21 20:57:15 -0300

    h3 titulo menu

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ except FileNotFoundError:
 
 
 with st.sidebar:
-    st.markdown("<h1 style='text-align: center; font-size: 40px; color: white'>📊 Dados da Violência no Brasil</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; font-size: 40px; color: white'>📊 Dados da Violência no Brasil</h3>", unsafe_allow_html=True)
     st.header("Dados Violência Brasil")
     
     # --- CÓDIGO CSS ATUALIZADO ---
