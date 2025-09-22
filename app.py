cee6167779a9594aa3723ef7edeb9941074c416b
flaviasantos190885 <flaviasantos190885@gmail.com>
2025-09-21 21:11:16 -0300

    texto

--- app.py
+++ app.py
@@ -127,15 +127,11 @@ with st.sidebar:
     
     st.info("Este painel oferece uma análise visual dos dados de violência e um módulo para estimativas futuras. ")
     
-    st.info(
-    "Trabalho de Conclusão de Curso (TCC) em Gestão da Tecnologia da Informação (GTI)\n"
-    "IF Sudeste MG - Campus Muriaé\n"
-    "Desenvolvido por Flavia Santos"
-    )
-    
     st.markdown("""
-    <div style="font-size: 14px; color: #D3D3D3; line-height: 1.6;">
-        Trabalho de Conclusão de Curso (TCC) em Gestão da Tecnologia da Informação (GTI)
+    <div style="font-size: 8px; color: #D3D3D3; line-height: 1.6;">
+        Trabalho de Conclusão de Curso (TCC) 
+        <br>
+        Gestão da Tecnologia da Informação (GTI)
         <br>
         IF Sudeste MG - Campus Muriaé
         <br>
